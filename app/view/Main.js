915f5693c94c3f860501fe826c1b1a4d74f8375a
Marlen Jacob <majbootred@gmail.com>
2014-05-21 19:09:04 +0200

    final version
    with email adress

--- app/view/Main.js
+++ app/view/Main.js
@@ -28,7 +28,7 @@ Ext.define('weatherimages.view.Main', {
             }]
         },
         {
-            html: '<div class=\"container\"><div class=\"main\">Weather Images App by Marlen Jacob. The app uses APIs from <a href=\"http://www.weatheronline.com\">weatheronline.com</a> and <a href=\"http://www.flickr.com\">flickr.com.</a> The weather data are based on the nearest weather checkpoint. The background image is the latest image on flickr.com which was taken in 1 km arround the device. The default background image is my home alley in Hamburg, Germany.</div></div>'
+            html: '<div class=\"container\"><div class=\"main\" style=\"font-size:1em\">Weather Images App by Marlen Jacob (<a href=\"mailto:maj@bootred.de\">maj@bootred.de</a>). The app uses APIs from <a href=\"http://www.weatheronline.com\">weatheronline.com</a> and <a href=\"http://www.flickr.com\">flickr.com.</a> The weather data are based on the nearest weather checkpoint. The background image is the latest image on flickr.com which was taken in 1 km arround the device. The default background image is my home alley in Hamburg, Germany.</div></div>'
         },{
             itemId: 'mainview',
             cls: 'textview'
